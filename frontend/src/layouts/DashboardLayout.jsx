11e8bf2ef326f3282e9f2e8d0a02ee108692b2eb
Ganesh-Karanam <ganeshprofilegk@gmail.com>
2026-06-28 22:28:17 +0530

    Rename TempSideNav to SideNav

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-import SideNav from "../components/TempSideNav";
+import SideNav from "../components/SideNav";
 import Footer from "../components/Footer";
 
 function DashboardLayout() {
